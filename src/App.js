92bf2f0a8e10b559ec3a8d02cbfcdf8085119165
AnnaBananaFanta <Anna.lattanzi.6066@gmail.com>
2025-08-09 01:47:08 -0500

    basics

--- src/App.js
+++ src/App.js
@@ -6,8 +6,31 @@ function App() {
 //const url = 'https://api.openweathermap.org/data/2.5/weather?q=dallas&appid=91c0215cc678a5541031762d41f40490'
 
   return (
-    <div className="App">
-
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="temp">
+            <h1>60°F</h1>
+          </div>
+          <div className="description">
+            <p>Clouds</p>
+          </div>
+        </div>
+        <div className="bottom">
+          <div className="feels">
+            <p>65°F</p>
+          </div>
+          <div className="humidity">
+            <p>20%</p>
+          </div>
+          <div className="wind">
+            <p>12 MPH</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
